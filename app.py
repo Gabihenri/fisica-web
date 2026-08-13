d6977bf0cd69bee945d55bfc8bfdb09e18827e87
Gabihenri <51890771+Gabihenri@users.noreply.github.com>
2026-08-13 13:43:42 -0300

    feat: expor laboratorio de sensores em rota web

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+from flask import render_template
+
 from app_core import app
+
+
+@app.route("/laboratorio-sensores")
+def laboratorio_sensores():
+    return render_template("laboratorio_sensores.html")
 
 
 if __name__ == "__main__":
