f70ccecb757bf3b7e85835ed5f584c852f99f055
Gabihenri <51890771+Gabihenri@users.noreply.github.com>
2026-08-19 07:03:39 -0300

    refactor: isolar integracao do catalogo experimental

---
+++ __init__catalogo.py
@@ -0,0 +1,7 @@
+"""Integração opcional do catálogo de novos experimentos.
+
+Este módulo é importável pela aplicação principal sem alterar o núcleo dos
+experimentos existentes.
+"""
+
+from routes_experimentos_novos import bp_experimentos_novos
